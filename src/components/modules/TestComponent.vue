9b71726ccc8190b23fe05cb05f9f1d96198003f8
楠瀬光 <kusunosehikari@kusunosehikarinoMacBook-Pro.local>
2020-10-31 12:55:13 +0900

    add,Hellow World!!の追記

---
+++ src/components/modules/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{testData}}
+  </div>
+</template>
+
+<script>
+export default {
+name:'TestComponent',
+data(){
+  return{
+    testData:'Hellow World!!'
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
